067fd8ebc178c7f20e8f1059b666df3ea6737325
Andrew Blake <drewblake1974@gmail.com>
2018-06-16 09:28:23 -0400

    original master branch

---
+++ index.js
@@ -0,0 +1,49 @@
+const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(userInput, callback) {
+  const query = {
+    part: 'snippet',
+    type: 'video',
+    key: 'AIzaSyBAF_VBTljjbBf2BIhP8mXReU6pdHIPrIA',
+    q: `${userInput}`
+  }
+  $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
+}
+
+//use this to link to video from image url.
+//last part after query is items.videoId in api object.
+//https://www.youtube.com/results?search_query=ws1o-N024es
+
+function renderResult(result) {
+  const videoLink = `https://www.youtube.com/results?search_query=${result.id.videoId}`;
+  return `<div class='results'><a href="${videoLink}"><img src="${result.snippet.thumbnails.medium.url}" alt='${result.snippet.title}'/></a></div>`;
+}
+
+function displayYouTubeData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  
+  //console.log(data.items[0].snippet.title);
+  $('.js-search-results').html(results);
+}
+
+function getSubmitDisplay () {
+ /* $('js-search-form').submit(function(event) {
+    event.preventDefault();
+    const queryTarget = $(this).find('.js-query');
+    const query = queryTarget.val();
+    queryTarget.val('');
+    getDataFromApi(query, displayYouTubeData);
+  });*/
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayYouTubeData);
+    console.log(query);
+    
+  });
+}
+
+$(getSubmitDisplay);
