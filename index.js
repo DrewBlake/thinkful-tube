3bc4d2b93338d305db0ff4cc40b4dd6b66046582
Andrew Blake <drewblake1974@gmail.com>
2018-06-16 09:37:54 -0400

    deleted __replit_main_ empty file

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function getDataFromApi(userInput, callback) {
 
 function renderResult(result) {
   const videoLink = `https://www.youtube.com/results?search_query=${result.id.videoId}`;
-  return `<div class='results'><a href="${videoLink}"><img src="${result.snippet.thumbnails.medium.url}" alt='${result.snippet.title}'/></a></div>`;
+  return `<div class='results' aria-live='assertive'><a href="${videoLink}"><img src="${result.snippet.thumbnails.medium.url}" 
+          alt='${result.snippet.title}'/></a></div>`;
 }
 
 function displayYouTubeData(data) {
